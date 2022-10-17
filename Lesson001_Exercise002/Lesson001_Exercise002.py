d38681775060879e97552a323c92eb2586acbe47
Ilya Kozlov Mac <ilya.kozlovmk91@gmail.com>
2022-10-17 13:57:13 +0300

    Removed diblicated folders_17/10/22

--- Lesson001_Exercise002/Lesson001_Exercise002.py
+++
@@ -1,9 +0,0 @@
-# Write a code which will check either ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z is TRUE or FALSE
-
-x = int(input("Enter X: "))
-y = int(input("Enter Y: "))
-z = int(input("Enter Z: "))
-
-c = ~(x + y + z) == ~(x) * ~(y) * ~(z)
-
-print(c)
