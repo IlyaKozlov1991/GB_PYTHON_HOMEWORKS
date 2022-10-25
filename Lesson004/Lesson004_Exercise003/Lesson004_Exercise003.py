e649433ff5f5c3ec171bafdeb1e4967896d78bd9
Ilya Kozlov Mac <ilya.kozlovmk91@gmail.com>
2022-10-25 23:29:53 +0300

    Correction of Lesson004_Exercise003_25/10/22

--- Lesson004/Lesson004_Exercise003/Lesson004_Exercise003.py
+++ Lesson004/Lesson004_Exercise003/Lesson004_Exercise003.py
@@ -14,27 +14,35 @@ k = int(randint(2, 10))
 print(f'\nYour random degree: {k}\n')
 
 def add_coefficients(list, n): # Function to ceate a list of coefficients
-    for i in range(n):
+    for i in range(n + randint(0, 5)): # Random number of coefficients
         a = int(randint(0, 100))
         list.append(a)
 
 
-def polym(list, n): # Function to create polynominal of degree
+def polym(list1, list2, n): # Function to create polynominal of degree
     inter_list = []
-    for i in range(n):
-        while n >= 0 and n != 1:
-            # print(n)
-            # print(i)
-            inter_list.append(f'{list1[i]}*x^{n}')
-            list.append(f'{list1[i]}*x^{n}')
-            n = n - 1
-            # print(inter_list)
-            i += 1
-        else:
-            # print(n)
-            inter_list.append(list1[i])
-            list.append(list1[i])
-            break
+    for i in range(len(list1)):
+        while n >= 0:
+            if n > 1:
+                # print(f'if i: {i}')
+                inter_list.append(f'{list1[i]}*x^{n}')
+                list2.append(f'{list1[i]}*x^{n}')
+                n = n - 1
+                # print(inter_list)
+                i += 1
+            elif n == 1:
+                # print(f'elif i: {i}')
+                inter_list.append(f'{list1[i]}*x')
+                list2.append(f'{list1[i]}*x')
+                n = n -1
+                i += 1
+            elif n == 0 and i <= len(list1) - 1:
+                # print(f'!!! i {i}')
+                inter_list.append(f'{list1[i]}')
+                list2.append(f'{list1[i]}')
+                i += 1
+                # print(f'!!! i: {i} inter_list: {inter_list}')
+            break # VERY IMPORTANT!!! Otherwise  cycle will try to go over len(list1)
     # print(inter_list)
     # print(list)
 
@@ -44,7 +52,7 @@ list2 = []
 
 add_coefficients(list1, k)
 
-polym(list2, k)
+polym(list1, list2, k)
 
 str1 = ""
 
@@ -53,7 +61,7 @@ for i in range(len(list2)):
 
 str1 = str1[:len(str1) - 2:] # Slice to remove extra " + " in str
 
-print("Your polynominal of degree:")
+print("Your polynomial of degree:")
 print(f'\n{str1} = 0\n')
 
 # my_date = date.today()
@@ -61,5 +69,5 @@ print(f'\n{str1} = 0\n')
 date_time = datetime.now()
 
 file = open("Lesson004_Ex003.txt", "a")
-file.write(f'\nYour polynominal of degree:\n{date_time.day}/{date_time.month}/{date_time.year} {date_time.hour}:{date_time.minute}:{date_time.second}: {str1} = 0\n')
+file.write(f'\nYour random degree: {k}\nYour polynomial of degree:\n{date_time.day}/{date_time.month}/{date_time.year} {date_time.hour}:{date_time.minute}:{date_time.second}: {str1} = 0\n')
 file.close()
