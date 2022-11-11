fcb3a17bd074cd7c4c64b09629ca1b811157ac0f
Ilya Kozlov <ilyakozlovmk91@gmail.com>
2022-11-11 19:08:13 +0300

    Lesson005_Exercise002 in progress_11.11.22

--- Lesson005/Lesson005_Exercise002/Lesson005_Exercise002.py
+++ Lesson005/Lesson005_Exercise002/Lesson005_Exercise002.py
@@ -1,60 +1,31 @@
 # RLE algorythm
 
 str1 = 'abcdefffghjkklmnooo'
-
-counter = 1
-var = ''
-coef = ''
-temp1 = ''
-list1 = []
-i = 0
 str2 = ''
-print(f'len(str1): {len(str1)}')
-print(f'len(str1) - 1 {len(str1) - 1}')
+var = ''
+counter = 1
+coef = ''
+list1 = []
 
-
-for i in range (len(str1) - 1):
+for i in range (len(str1)):
     var = str1[i]
-    if var != str1[i - 1] and var != str1[i + 1]:
-        print(f'if i: {i}')
-        print(f'if counter: {counter}')
-        # counter = 1
-        coef = var
-        list1.append(coef)
-        print(f'if list1: {list1}')
-    elif var == str1[i - 1]: 
-        print(f'elif1 i: {i}')
-        # var == str1[i - 1]
-        counter +=1
-        coef = f'{counter}{var}'
-        temp1 = coef
-        print(f'elif1 list1: {list1}')
-        print(f'elif1 counter: {counter}')
-    elif var != str1[i - 1] and counter > 1:
-        print(f'elif2 i: {i}')
-        list1.append(temp1)
-        counter = 1
-        print(f'elif2 list1: {list1}')
-print(list1)
+    if var != str1[i - 1]: # Does current value match previous one?
+        if var == str1[i + 1]: # And does it match next one?
+            counter += 1
+            coef = f'{counter}{var}'
+            list1.append(coef)
+            # print(f'if i: {i}')
+            # print(f'if counter: {counter}')
+            # print(f'if list1: {list1}')
+        else:
+            counter = 1
+            coef = var
+            list1.append(coef)
+            # print(f'else i: {i}')
+            # print(f'else counter: {counter}')
+            # print(f'else list1: {list1}')
 
-# def encode(message):
-# 	encoded_message = ""
-# 	i = 0
-
-# 	while (i <= len(message)-1):
-# 		count = 1
-# 		ch = message[i]
-# 		j = i
-# 		while (j < len(message)-1):
-# 			if (message[j] == message[j+1]):
-# 				count = count+1
-# 				j = j+1
-# 			else:
-# 				break
-# 		encoded_message=encoded_message+str(count)+ch
-# 		i = j+1
-# 	return encoded_message
-
-# #Provide different values for message and test your program
-# encoded_message=encode("ABBBBCCCCCCCCAB")
-# print(encoded_message)
+for var in list1:
+    str2 = str2 + var
+print(f'\nOriginal string:\n{str1}')
+print(f'\nFormated string:\n{str2}\n')
