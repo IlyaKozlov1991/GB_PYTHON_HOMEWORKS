82d530570660ca879f1a59969bd88583f82e2fdd
Ilya Kozlov <ilyakozlovmk91@gmail.com>
2022-11-29 20:55:57 +0300

    Lesson006_Exercise002 done_29/11/22

--- Lesson006/Lesson006_Exercise002.py
+++ Lesson006/Lesson006_Exercise002.py
@@ -1,14 +1,14 @@
 # Write a program to calculate an arithmetic expression given by a string. Use the +,-,/,* operations. operation priority is standard.
 
-user_str = input('Enter your expression: ')
+user_str = input('\nEnter your expression: ')
 
 my_list = []
 
-for char in user_str: # Remove space between symbols, if User put some
+for char in user_str: # Remove space between symbols, if User put some, and append values to list
     if char != ' ':
-        my_list.append(char)
+        my_list.append(char) 
 
-for i in range(len(my_list)):
+for i in range(len(my_list)): # Convert items of the list into INT, if items are numbers
     try:
         my_list[i] = int(my_list[i])
     except ValueError:
@@ -47,5 +47,4 @@ for j in range(count_add + 1):
                     my_list.pop(i)
                     break
 result = int(my_list[0])
-
-print(f'{user_str} = {result}')
+print(f'\nResult:\n{user_str} = {result}\n')
